50a8008a69b4526a7227b1696e7518720ec3bd6e
Dharani-CCT <dharanikumar.cct@gmail.com>
2026-08-18 12:35:19 +0530

    Script update

--- scripts/health-check.sh
+++ scripts/health-check.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-URL="http://localhost:4040/health"
-
-echo "    "
+echo ""
 echo "ShareBox Health Check"
-echo "    "
+echo ""
 
-if curl -fs "$URL" > /dev/null; then
-
+if curl -fs http://localhost:4040/health; then
+    echo ""
     echo "Application Status : UP"
     echo "Health Check       : PASSED"
-
     exit 0
-
 else
-
+    echo ""
     echo "Application Status : DOWN"
     echo "Health Check       : FAILED"
-
     exit 1
-
 fi
